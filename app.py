2d3411f09a56c1afd5dc31339cd96dedace43d9e
Noah Platton <noahplatton@gmail.com>
2022-03-22 15:30:08 +0000

    Add database

--- app.py
+++ app.py
@@ -4,13 +4,18 @@ from flask_cors import CORS
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/users", methods=["GET", "POST"])
 def index():
     return "Hello World"
 
 
+# @app.route("/users/<user>", methods=["GET"])
+# def handle_user(user):
+    
 
-
+# @app.route("/users/<user>/fights", methods=["GET", "POST"])
+# def handle_fights(user):
+    
 
 if __name__ == "__main__":
     app.run(debug = True)
