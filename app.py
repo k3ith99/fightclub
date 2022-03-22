71ba01eb1958e5390009c1fde9d36f498e918757
zmahmood98 <zahra.mahmood98@hotmail.co.uk>
2022-03-22 15:39:08 +0000

    update pipfile

--- app.py
+++ app.py
@@ -1,16 +1,14 @@
-from flask import Flask, escape,request,jsonify
+from flask import Flask, escape, request, jsonify
 from flask_cors import CORS
+
 
 app = Flask(__name__)
 
-
 @app.route("/")
 def index():
-    return "Hello World"
-
-
-
-
+    return "Hello World"  
 
 if __name__ == "__main__":
     app.run(debug = True)
+
+
